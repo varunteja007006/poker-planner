f922f5d5893b0c6b57bd968449a179bb13309c00
varun teja <varunteja007006@gmail.com>
2025-10-02 21:41:19 +0530

    ✅ : dark mode ui fix for participant card

--- poker_planner_vite_frontend/src/components/features/poker-board/ParticipantCard.tsx
+++ poker_planner_vite_frontend/src/components/features/poker-board/ParticipantCard.tsx
@@ -14,7 +14,7 @@ export default function ParticipantCard({
   isOwner?: boolean;
 }>) {
   return (
-    <div className="border-primary/25 flex w-full flex-row items-center justify-between gap-2 overflow-hidden rounded-md border bg-white p-2 pr-4 pl-2 shadow dark:bg-black">
+    <div className="border-primary/25 flex w-full flex-row items-center justify-between gap-2 overflow-hidden rounded-md border bg-white p-2 pr-4 pl-2 shadow dark:bg-secondary">
       <div className="flex flex-row items-center gap-2">
         <CircleUserRound className={cn("shrink-0 text-primary")} />
         <div className="flex flex-col items-start">
