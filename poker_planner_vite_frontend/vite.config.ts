bfa815caeaacc29bd98512be5dad09cbb67263e4
varun teja <varunteja007006@gmail.com>
2025-10-02 18:09:04 +0530

    ✅:update vite config

--- poker_planner_vite_frontend/vite.config.ts
+++ poker_planner_vite_frontend/vite.config.ts
@@ -6,11 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(),
     tailwindcss(),
   ],
   resolve: {
